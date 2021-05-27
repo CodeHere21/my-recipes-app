faf500119ce3d231e6c7bb6538342d90da2710cb
Lena <alena.litouka@gmail.com>
2021-05-26 23:15:35 -0400

    recipe model added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply test',
+      'https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/3DBSEVFZPUI6XPCKMKCJZ5WMVE.jpg'),
+    new Recipe('A Test Recipe', 'This is simply test',
+      'https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/3DBSEVFZPUI6XPCKMKCJZ5WMVE.jpg')
+  ];
 
   constructor() { }
 
